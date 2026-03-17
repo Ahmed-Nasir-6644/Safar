d4f182181e031077c83385dc1fad149bc086db1d
Ahmed-Nasir-6644 <ahmednasir1212a@gmail.com>
2026-03-17 14:15:32 +0500

    fixed email issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import AuthPage from './pages/AuthPage';
 import FindRoutePage from './pages/FindRoutePage'; // Keep existing import for now, will be replaced in route
